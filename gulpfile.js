5258f6059f01fa3c526d3fd92dcfbb1060fd8654
Georgiy <region4vosem@gmail.com>
2023-06-03 20:37:26 +0300

    some new files and upd

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,12 @@ gulp.task('server', function() {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
                 prefix: "",
@@ -28,8 +30,7 @@ gulp.task('styles', function() {
 })
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
